3932d2e1c11df0e7bad4d5d16e621cac082ef290
Sandro <sandro.luiz@hvex.com.br>
2021-12-12 20:11:25 -0300

    CONFIG API AND CARDS

---
+++ src/components/List/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ListCards = styled.div`
+    display: flex;
+    gap: 20px;
+    align-items: center;
+`
--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,40 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api.dailymotion.com'
 })
+
+
+const basicFetch = async (endpoint) => {
+  const req = await api.get(`${endpoint}`);
+  return req;
+};
+
+export default{
+  getHomeList: async () => {
+    return [
+      {
+        slug: 'Jesus',
+        title: 'Novela Jesus',
+        items: await basicFetch(`playlist/x6hnrv/videos`),
+      },
+      {
+        slug: 'Apocalipse',
+        title: 'Novela Apocalipse',
+        items: await basicFetch(`playlist/x54krc/videos`),
+      }, {
+        slug: 'Genesis',
+        title: 'Novela Genesis',
+        items: await basicFetch(`playlist/x70181/videos`),
+      },
+      {
+        slug: 'Amor',
+        title: 'Amor sem Igual',
+        items: await basicFetch(`playlist/x6jyfx/videos`),
+      },
+      {
+        slug: 'Topssima',
+        title: 'Topssíma',
+        items: await basicFetch(`playlist/x6a7co/videos`),
+      },
+    ]
+  }
+}
