76d3cbc2689ab7dcb9f3cb81472c5f925e1cf542
sandrocarvalho10 <sandrinho9797@outlook.com>
2022-04-18 18:08:55 -0300

    config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Header } from '../src/components/Header'
-import { Main } from '../src/components/Main'
+import { Header } from '../components/Header'
+import { Main } from '../components/Main'
 
 const Home: NextPage = () => {
   return (
