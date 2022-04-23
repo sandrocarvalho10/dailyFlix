f61bebebc6f403a339011d173ad4b6383b9fc010
sandrocarvalho10 <sandrinho9797@outlook.com>
2022-04-22 21:48:56 -0300

    config types

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,17 +3,18 @@ import { Card } from "../Card";
 import * as S from "./styles";
 import { useEffect, useState } from "react";
 import { List } from "../List";
+import { AxiosResponse } from "axios";
 
 export interface ListProps {
   slug: string,
   title: string,
   src: string,
-  items: any
+  items: AxiosResponse<any, any> | never
 
 }
 export const Main: React.FC = () => {
   const playlists = ["x6hnrv"];
-  const [lista, setLista] = useState<ListProps | never[]>([]);
+  const [lista, setLista] = useState<any>([]);
   const [playlist1, setPlaylist1] = useState([]);
 
   useEffect(() => {
@@ -32,7 +33,7 @@ export const Main: React.FC = () => {
   return (
     <S.Main className="container">
       {
-        lista.map((movie,key) => (
+        lista.map((movie: any,key: any) => (
           <S.ContainedTitle key={key}>
             <S.Title>{movie.title}</S.Title>
             <List items={movie.items} />
