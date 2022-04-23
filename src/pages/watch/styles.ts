8b9d563bfe7be8d8c063356f8c1afd0b4554dc2a
sandrocarvalho10 <sandrinho9797@outlook.com>
2022-04-22 23:19:05 -0300

    fix app teste deploy

--- src/pages/watch/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Main } from "../../components/Main/styles";
-
-export const Container = styled(Main)`
-  width: 100%;
-  max-width: 1170px;
-  margin: 0 auto;
-`;
