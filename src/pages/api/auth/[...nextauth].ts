10cf7e146451875dab7df654139a77bd0b48b4ac
Sandro <sandrinho9797@outlook.com>
2023-03-28 23:49:31 -0300

    removendo env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
